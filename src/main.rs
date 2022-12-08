4efe723d98622561911076d62351fca7bdbcfbb0
Hinterberger-Thomas <thomas.hinterberger.eu@gmail.com>
2022-12-08 20:00:57 +0100

    read args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use core::panic;
+use std::{env, fs};
+
+fn main(){
+    let args: Vec<String> = env::args().collect();
+
+    let file_path = match args.get(1){
+        Some(path) => path,
+        None => panic!("you need to provide a filepath")
+    };
+
+    let conents = fs::read_to_string(file_path)
+    .unwrap();
+
+    println!("{}",conents);
+    
 }
